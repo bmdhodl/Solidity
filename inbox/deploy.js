1372ea675497ab7adf11bd54e73a648118b3e241
Pat <patrickhughes013@gmail.com>
2021-09-09 21:17:35 -0500

    API & Wallet Connections
    Infura API connected.
    
    Truffle Wallet setup.

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,10 @@
 // deploy code will go here
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'october celery firm raise nuclear gate view someone sun fringe chunk teach',
+    'https://rinkeby.infura.io/v3/df277fabdd07466cabccf3a8d39b6931'
+);
+const web3 = new Web3(provider);
