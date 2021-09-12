f636508fc408b99212f4f6f52fc18a6deb8a8dc1
Pat <patrickhughes013@gmail.com>
2021-09-11 20:08:20 -0500

    Use Remix to interact with contract easily

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.17;
 // linter warnings (red underline) about pragma version can igonored!
-
+// Use this code in Remix to easily interact with this code
 // contract code will go here
 contract Inbox {
     string public message;
